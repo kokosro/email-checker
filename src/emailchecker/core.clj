21d04f8b73a58895d7062de5816aef1d3535df3e
Robert Doroftei <robert@kokos.ro>
2017-04-20 00:18:15 +0300

    re-added QUIT message

--- src/emailchecker/core.clj
+++ src/emailchecker/core.clj
@@ -137,9 +137,8 @@
                         [#(do (log/info (format "checking %s" %))
                             (format "EHLO %s" (or (first helo) "localhost")))
                          #(format "MAIL FROM:<%s>" %)
-                         #(format "RCPT TO:<%s>" %)
-                         ])
-              ;_ (ms/put! socket "QUIT")
+                         #(format "RCPT TO:<%s>" %)])
+              _ (ms/put! socket "QUIT")
               _ (get-next-response socket timeout)
               _ (.close socket)]
         validity)
